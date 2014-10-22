bb2e291eb6f3bbfa9e4478ee15165e6803b207b7
Leon Morris <lmuk2k@hotmail.com>
2014-10-22 23:53:24 +0100

    added initial settings for home env

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
+	'home' => array('home-desktop'),
+        'work' => array('work-vm')
 
 ));
 
