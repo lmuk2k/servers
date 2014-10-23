53f7b5f98019630a8dc0ead9f7b5ad972f9dfa4a
Leon Morris <lmuk2k@gmail.com>
2014-10-23 14:54:11 +0100

    added static routing

--- app/routes.php
+++ app/routes.php
@@ -1,23 +1,30 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the Closure to execute when that URI is requested.
+  |
+ */
 
-Route::get('/', function()
-{
-	return View::make('hello');
+// ===============================================
+// STATIC PAGES ==================================
+// ===============================================
+// show a static view for the home page (app/views/home.blade.php)
+Route::get('/', function() {
+    return View::make('home');
 });
 
-Route::get('websites', function()
-{
+// about page (app/views/about.blade.php)
+Route::get('about', array('as' => 'about', function() {
+        return View::make('about');
+    }));
+
+Route::get('websites', function() {
     $websites = Website::all();
 
     return View::make('websites')->with('websites', $websites);
