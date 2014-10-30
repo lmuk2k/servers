2f79751a861bcd64ba113c2c5864022965b8edd4
Leon Morris <lmuk2k@gmail.com>
2014-10-30 16:14:06 +0000

    cover image

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.default')
 @section('content')
 
-<div id="wrapper">
-    <div class="row">
-        <div class="large-12 columns">
-            <p>Servers blah blah</p>
-        </div>
+<div class="row">
+    <div class="large-12 columns">
+        <p>Servers blah blah</p>
     </div>
 </div>
 
