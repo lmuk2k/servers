c46d3d54966b568126014a21cea0f1cef4560447
Leon Morris <lmuk2k@hotmail.com>
2014-11-05 23:42:35 +0000

    Added Foundation SASS

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>Servers | Welcome</title>
-<link rel="stylesheet" href="/css/servers.css" />
+<link rel="stylesheet" href="/css/normalize.css" />
+<link rel="stylesheet" href="/css/app.css" />
 <script src="js/vendor/modernizr.js"></script>
